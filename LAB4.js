a51d7e182465f08c56d9fd3bc8c04adc39b2e0fd
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-01-28 15:57:11 +0200

    adding while loop and for loop

--- LAB4.js
+++ LAB4.js
@@ -13,13 +13,13 @@ var image='';
 var ph1 = document.createElement("img");
 var pho = document.createElement("img");
 
-var x = prompt('how many times you went to these planets?')
+var x = prompt('how many times you went to see these planets?')
 
-
+alert('you are lucky ');
 for(n=0 ; n<x ; n++ ){
 
 if (planet == mars) {
-    //alert('nice choice');
+    //alert('you are lucky ');
    
     image =image + '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg">'
     console.log(image)
@@ -27,7 +27,7 @@ if (planet == mars) {
     document.write('<h3>' + 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".' + '</h3>');
 }
     else if (planet == saturn) {
-         //alert('nice choice')
+        //alert('you are lucky ');
          image = image + '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Saturn%2C_Earth_size_comparison2.jpg/220px-Saturn%2C_Earth_size_comparison2.jpg ">'
          console.log(image)
          document.write('<h3>' + 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine times that of Earth.' + '</h3>');
